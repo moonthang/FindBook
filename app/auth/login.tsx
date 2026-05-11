3ceab14be26449c8975a14b9e35a8f8b867c3b5e
Gavy <gavyadiaz@ucompensar.edu.co>
2026-05-11 11:59:38 -0500

    Ajuste visual flecha auth responsive

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,75 +1,139 @@
 import { Ionicons } from '@expo/vector-icons';
 import { Link } from 'expo-router';
 import React from 'react';
-import { ActivityIndicator, Image, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import {
+    ActivityIndicator,
+    Image,
+    ScrollView,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+} from 'react-native';
+
 import { styles } from '../../constants/styleAuth';
 import { useLogin } from '../controllers/authController';
 
 export default function Login() {
-    const { email, setEmail, password, setPassword, loading, handleLogin } = useLogin();
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    loading,
+    handleLogin,
+  } = useLogin();
 
-    return (
-        <ScrollView style={styles.contentContainer} contentContainerStyle={styles.container}>
-            <View>
-                <Link href="/" asChild>
-                    <TouchableOpacity style={styles.btnBack}>
-                        <Ionicons name="chevron-back-circle-outline" size={32} color="black" />
-                    </TouchableOpacity>
+  return (
+    <View style={styles.contentContainer}>
+      <Link href="/" asChild>
+        <TouchableOpacity style={styles.btnBack} activeOpacity={0.8}>
+          <Ionicons
+  name="chevron-back-circle-outline"
+  size={38}
+  color="black"
+/>
+        </TouchableOpacity>
+      </Link>
+
+      <ScrollView
+        style={{ flex: 1, width: '100%' }}
+        contentContainerStyle={styles.container}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={styles.content}>
+          <View style={styles.containerLogo}>
+            <View style={styles.logoContainer}>
+              <Image
+                source={require('../../assets/images/favicon.png')}
+                style={styles.imgLogo}
+              />
+
+              <Text style={styles.txtLogo}>FindBook</Text>
+            </View>
+
+            <Text style={styles.subtitle}>
+              Inicie sesión para continuar y encontrar su próxima lectura.
+            </Text>
+          </View>
+
+          <View style={styles.cardContainer}>
+            <View style={styles.form}>
+              <Text style={styles.label}>Correo electrónico</Text>
+
+              <TextInput
+                style={styles.input}
+                placeholder="correo@ejemplo.com"
+                placeholderTextColor="gray"
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+              />
+
+              <View style={styles.passwordHeader}>
+                <Text style={styles.label}>Contraseña</Text>
+
+                <Link href=".." style={styles.txtForgot}>
+                  ¿Olvidaste tu contraseña?
                 </Link>
+              </View>
+
+              <View style={styles.passwordContainer}>
+                <TextInput
+                  style={[styles.input, { flex: 1, borderWidth: 0, marginBottom: 0 }]}
+                  placeholder="Ingrese su contraseña"
+                  placeholderTextColor="gray"
+                  value={password}
+                  onChangeText={setPassword}
+                  secureTextEntry
+                />
+              </View>
+
+              <View style={styles.btnLoginContainer}>
+                <TouchableOpacity
+                  style={[styles.btnLogin, loading && { opacity: 0.8 }]}
+                  activeOpacity={0.8}
+                  onPress={handleLogin}
+                  disabled={loading}
+                >
+                  {loading ? (
+                    <ActivityIndicator color="#fff" />
+                  ) : (
+                    <Text style={styles.btnLoginTxt}>Iniciar Sesión</Text>
+                  )}
+                </TouchableOpacity>
+              </View>
             </View>
 
-            <View style={styles.content}>
-                <View style={styles.containerLogo}>
-                    <View style={styles.logoContainer}>
-                        <Image source={require('../../assets/images/favicon.png')} style={styles.imgLogo} />
-                        <Text style={styles.txtLogo}>FindBook</Text>
-                    </View>
-                    <Text style={styles.subtitle}>Inicie sesión para continuar y encontrar su próxima lectura.</Text>
-                </View>
+            <View style={styles.dividerContainer}>
+              <View style={styles.line} />
+
+              <Text style={styles.txtDivider}>o inicia sesión con</Text>
+
+              <View style={styles.line} />
             </View>
 
-            <View style={styles.content}>
-                <View style={styles.cardContainer}>
-                    <View style={styles.form}>
-                        <Text style={styles.label}>Correo electrónico</Text>
-                        <TextInput style={styles.input} placeholder="correo@ejemplo.com" placeholderTextColor={'gray'} value={email} onChangeText={setEmail} keyboardType="email-address" autoCapitalize="none" />
-                        <View style={styles.passwordHeader}>
-                            <Text style={styles.label}>Contraseña</Text>
-                            <Link href=".." style={styles.txtForgot}>¿Olvidaste tu contraseña?</Link>
-                        </View>
-                        <View style={styles.passwordContainer}>
-                            <TextInput style={[styles.input, { flex: 1, borderWidth: 0 }]} placeholder="Ingrese su contraseña" placeholderTextColor={'gray'} value={password} onChangeText={setPassword} secureTextEntry />
-                        </View>
-                        <TouchableOpacity style={[styles.btnLogin, loading && { opacity: 0.8 }]} activeOpacity={0.8} onPress={handleLogin} disabled={loading}>
-                            {loading ? (
-                                <ActivityIndicator color="#fff" />
-                            ) : (
-                                <Text style={styles.btnLoginTxt}>Iniciar Sesión</Text>
-                            )}
-                        </TouchableOpacity>
-                    </View>
+            <View style={styles.socialContainer}>
+              <TouchableOpacity style={styles.btnSocial}>
+                <Text style={styles.txtSocial}>Google</Text>
+              </TouchableOpacity>
 
-                    <View style={styles.dividerContainer}>
-                        <View style={styles.line} />
-                        <Text style={styles.txtDivider}>o inicia sesión con</Text>
-                        <View style={styles.line} />
-                    </View>
-
-                    <View style={styles.socialContainer}>
-                        <TouchableOpacity style={styles.btnSocial}>
-                            <Text style={styles.txtSocial}>Google</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.btnSocial}>
-                            <Text style={styles.txtSocial}>Apple</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
+              <TouchableOpacity style={styles.btnSocial}>
+                <Text style={styles.txtSocial}>Apple</Text>
+              </TouchableOpacity>
             </View>
+          </View>
 
-            <View style={styles.contentSecundary}>
-                <Text style={styles.subtitle}>¿No tienes una cuenta?</Text>
-                <Link href="/auth/register" style={styles.txtForgot}>Regístrate aquí</Link>
-            </View>
-        </ScrollView>
-    );
+          <View style={styles.contentSecundary}>
+            <Text style={styles.subtitle}>¿No tienes una cuenta?</Text>
+
+            <Link href="/auth/register" style={styles.txtForgot}>
+              Regístrate aquí
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
--- app/auth/register.tsx
+++ app/auth/register.tsx
@@ -1,141 +1,208 @@
 import { Ionicons } from '@expo/vector-icons';
 import { Link } from 'expo-router';
 import React from 'react';
-import { ActivityIndicator, Image, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+import {
+    ActivityIndicator,
+    Image,
+    ScrollView,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+} from 'react-native';
+
 import { styles } from '../../constants/styleAuth';
 import { useRegister } from '../controllers/authController';
 
 export default function Register() {
-    const {
-        name, setName, email, setEmail, password, setPassword,
-        confirmPassword, setConfirmPassword, birthDate,
-        loading, handleDateChange, handleRegister
-    } = useRegister();
+  const {
+    name,
+    setName,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    confirmPassword,
+    setConfirmPassword,
+    birthDate,
+    loading,
+    handleDateChange,
+    handleRegister,
+  } = useRegister();
 
-    return (
-        <ScrollView style={styles.contentContainer} contentContainerStyle={styles.container}>
-            <View>
-                <Link href="/" asChild>
-                    <TouchableOpacity style={styles.btnBack}>
-                        <Ionicons name="chevron-back-circle-outline" size={32} color="black" />
-                    </TouchableOpacity>
-                </Link>
+  return (
+    <View style={styles.contentContainer}>
+      <Link href="/" asChild>
+        <TouchableOpacity style={styles.btnBack} activeOpacity={0.8}>
+          <Ionicons
+  name="chevron-back-circle-outline"
+  size={38}
+  color="black"
+/>
+        </TouchableOpacity>
+      </Link>
+
+      <ScrollView
+        style={{ flex: 1, width: '100%' }}
+        contentContainerStyle={styles.container}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={styles.content}>
+          <View style={styles.containerLogo}>
+            <View style={styles.logoContainer}>
+              <Image
+                source={require('../../assets/images/favicon.png')}
+                style={styles.imgLogo}
+              />
+
+              <Text style={styles.txtLogo}>FindBook</Text>
             </View>
 
-            <View style={styles.content}>
-                <View style={styles.containerLogo}>
-                    <View style={styles.logoContainer}>
-                        <Image source={require('../../assets/images/favicon.png')} style={styles.imgLogo} />
-                        <Text style={styles.txtLogo}>FindBook</Text>
-                    </View>
-                    <Text style={styles.subtitle}>Crea una cuenta y únete a la comunidad de amantes de los libros.</Text>
-                </View>
+            <Text style={styles.subtitle}>
+              Crea una cuenta y únete a la comunidad de amantes de los libros.
+            </Text>
+          </View>
+
+          <View style={styles.cardContainer}>
+            <View style={styles.form}>
+              <Text style={styles.label}>Nombre</Text>
+
+              <TextInput
+                style={styles.input}
+                placeholder="Ingrese su nombre"
+                placeholderTextColor="gray"
+                autoCapitalize="words"
+                value={name}
+                onChangeText={setName}
+              />
+
+              <Text style={styles.label}>Correo electrónico</Text>
+
+              <TextInput
+                style={styles.input}
+                placeholder="correo@ejemplo.com"
+                placeholderTextColor="gray"
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+              />
+
+              <Text style={styles.label}>Fecha de nacimiento</Text>
+
+              <TextInput
+                style={styles.input}
+                placeholder="DD-MM-AAAA"
+                placeholderTextColor="gray"
+                value={birthDate}
+                onChangeText={handleDateChange}
+                keyboardType="number-pad"
+                maxLength={10}
+              />
+
+              <View style={styles.passwordHeader}>
+                <Text style={styles.label}>Contraseña</Text>
+              </View>
+
+              <View style={styles.passwordContainer}>
+                <TextInput
+                  style={[
+                    styles.input,
+                    {
+                      flex: 1,
+                      borderWidth: 0,
+                      marginBottom: 0,
+                    },
+                  ]}
+                  placeholder="Ingrese su contraseña"
+                  placeholderTextColor="gray"
+                  value={password}
+                  onChangeText={setPassword}
+                  secureTextEntry
+                />
+              </View>
+
+              <View style={styles.passwordHeader}>
+                <Text style={styles.label}>
+                  Confirmar contraseña
+                </Text>
+              </View>
+
+              <View style={styles.passwordContainer}>
+                <TextInput
+                  style={[
+                    styles.input,
+                    {
+                      flex: 1,
+                      borderWidth: 0,
+                      marginBottom: 0,
+                    },
+                  ]}
+                  placeholder="Confirme su contraseña"
+                  placeholderTextColor="gray"
+                  value={confirmPassword}
+                  onChangeText={setConfirmPassword}
+                  secureTextEntry
+                />
+              </View>
+
+              <View style={styles.btnLoginContainer}>
+                <TouchableOpacity
+                  style={[
+                    styles.btnLogin,
+                    loading && { opacity: 0.8 },
+                  ]}
+                  activeOpacity={0.8}
+                  onPress={handleRegister}
+                  disabled={loading}
+                >
+                  {loading ? (
+                    <ActivityIndicator color="#fff" />
+                  ) : (
+                    <Text style={styles.btnLoginTxt}>
+                      Crear cuenta
+                    </Text>
+                  )}
+                </TouchableOpacity>
+              </View>
             </View>
 
-            <View style={styles.content}>
-                <View style={styles.cardContainer}>
-                    <View style={styles.form}>
-                        <Text style={styles.label}>Nombre</Text>
-                        <TextInput
-                            style={styles.input}
-                            placeholder="Ingrese su nombre"
-                            placeholderTextColor={'gray'}
-                            autoCapitalize="words"
-                            value={name}
-                            onChangeText={setName}
-                        />
+            <View style={styles.dividerContainer}>
+              <View style={styles.line} />
 
-                        <Text style={styles.label}>Correo electrónico</Text>
-                        <TextInput
-                            style={styles.input}
-                            placeholder="correo@ejemplo.com"
-                            placeholderTextColor={'gray'}
-                            value={email}
-                            onChangeText={setEmail}
-                            keyboardType="email-address"
-                            autoCapitalize="none"
-                        />
+              <Text style={styles.txtDivider}>
+                o regístrate con
+              </Text>
 
-                        <Text style={styles.label}>Fecha de nacimiento</Text>
-                        <TextInput
-                            style={styles.input}
-                            placeholder="DD-MM-AAAA"
-                            placeholderTextColor={'gray'}
-                            value={birthDate}
-                            onChangeText={handleDateChange}
-                            keyboardType="number-pad"
-                            maxLength={10}
-                        />
-
-                        <View style={styles.passwordHeader}>
-                            <Text style={styles.label}>Contraseña</Text>
-                        </View>
-
-                        <View style={styles.passwordContainer}>
-                            <TextInput
-                                style={[styles.input, { flex: 1, borderWidth: 0 }]}
-                                placeholder="Ingrese su contraseña"
-                                placeholderTextColor={'gray'}
-                                value={password}
-                                onChangeText={setPassword}
-                                secureTextEntry
-                            />
-                        </View>
-
-                        <View style={styles.passwordHeader}>
-                            <Text style={styles.label}>Confirmar contraseña</Text>
-                        </View>
-
-                        <View style={styles.passwordContainer}>
-                            <TextInput
-                                style={[styles.input, { flex: 1, borderWidth: 0 }]}
-                                placeholder="Confirme su contraseña"
-                                placeholderTextColor={'gray'}
-                                value={confirmPassword}
-                                onChangeText={setConfirmPassword}
-                                secureTextEntry
-                            />
-                        </View>
-
-                        <View style={styles.btnLoginContainer}>
-                            <TouchableOpacity
-                                style={[styles.btnLogin, loading && { opacity: 0.8 }]}
-                                activeOpacity={0.8}
-                                onPress={handleRegister}
-                                disabled={loading}
-                            >
-                                {loading ? (
-                                    <ActivityIndicator color="#fff" />
-                                ) : (
-                                    <Text style={styles.btnLoginTxt}>Crear cuenta</Text>
-                                )}
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-
-                    <View style={styles.dividerContainer}>
-                        <View style={styles.line} />
-                        <Text style={styles.txtDivider}>o regístrate con</Text>
-                        <View style={styles.line} />
-                    </View>
-
-                    <View style={styles.socialContainer}>
-                        <TouchableOpacity style={styles.btnSocial}>
-                            <Text style={styles.txtSocial}>Google</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.btnSocial}>
-                            <Text style={styles.txtSocial}>Apple</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
+              <View style={styles.line} />
             </View>
 
-            <View style={styles.contentSecundary}>
-                <Text style={styles.subtitle}>¿Ya tienes una cuenta?</Text>
-                <Link href="/auth/login" style={styles.txtForgot}>
-                    Inicia sesión aquí
-                </Link>
+            <View style={styles.socialContainer}>
+              <TouchableOpacity style={styles.btnSocial}>
+                <Text style={styles.txtSocial}>Google</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.btnSocial}>
+                <Text style={styles.txtSocial}>Apple</Text>
+              </TouchableOpacity>
             </View>
-        </ScrollView>
-    );
+          </View>
+
+          <View style={styles.contentSecundary}>
+            <Text style={styles.subtitle}>
+              ¿Ya tienes una cuenta?
+            </Text>
+
+            <Link
+              href="/auth/login"
+              style={styles.txtForgot}
+            >
+              Inicia sesión aquí
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
